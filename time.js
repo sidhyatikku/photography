56a752e71c7f77cfa53a1337034e0d6ddc2dc34a
Sidhya Tikku <sidhyaktikku@gmail.com>
2024-08-05 13:18:20 -0400

    add responsiveness

--- time.js
+++ time.js
@@ -5,11 +5,17 @@
          delTime: 'Asia/Kolkata',
          parTime: 'Europe/Paris',
          dxbTime: 'Asia/Dubai',
-         tokTime: 'Asia/Tokyo'
+         tokTime: 'Asia/Tokyo',
+         nycTime2: 'America/New_York',
+         delTime2: 'Asia/Kolkata',
+         parTime2: 'Europe/Paris',
+         dxbTime2: 'Asia/Dubai',
+         tokTime2: 'Asia/Tokyo'
      };
 
      for (const [id, timeZone] of Object.entries(timeZones)) {
          const timeElement = document.getElementById(id);
+         // console.log(timeElement);
          const now = new Date(new Date().toLocaleString('en-US', { timeZone }));
          const hours = String(now.getHours()).padStart(2, '0');
          const minutes = String(now.getMinutes()).padStart(2, '0');
@@ -18,5 +24,5 @@
  }
 
  // Update time every minute
- setInterval(updateTime, 60000);
+ setInterval(updateTime, 2000);
  updateTime(); // Initial call to set the time immediately
