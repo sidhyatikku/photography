a2b3b527d1e75a5c3136ea1c951bf5d0221c40f8
Sidhya Tikku <sidhyaktikku@gmail.com>
2025-06-09 13:39:39 -0400

    timer_cursor_new

--- main.js
+++ main.js
@@ -1,26 +1,68 @@
 const images = [
-'img/export/a1.jpg',
-'img/export/a2.jpg',
-'img/export/a3.jpg','img/export/bk.jpg',
-'img/export/a4.jpg',
-'img/export/a5.jpg',
-'img/export/a6.jpg',
-'img/export/a7.jpg',
-'img/export/a8.jpg',
-'img/export/a9.jpg',
-'img/export/a10.jpg',
-'img/export/a11.jpg',
-'img/export/a12.jpg',
-'img/export/a13.jpg',
-'img/export/a14.jpg',
-'img/export/a15.jpg',
-'img/export/a16.jpg',
-'img/export/a17.jpg',
-'img/export/a18.jpg',
-'img/export/a19.jpg',
-'img/export/a20.jpg',
-'img/export/a21.jpg','img/export/img-56.jpg','img/export/img-57.jpg',
-'img/export/img-1.jpg','img/export/img-12.jpg','img/export/img-13.jpg','img/export/img-14.jpg','img/export/img-15.jpg','img/export/img-16.jpg','img/export/img-17.jpg','img/export/img-18.jpg','img/export/img-19.jpg','img/export/img-2.jpg','img/export/img-22.jpg','img/export/img-23.jpg','img/export/img-24.jpg','img/export/img-26.jpg','img/export/img-27.jpg','img/export/img-28.jpg','img/export/img-3.jpg','img/export/img-32.jpg','img/export/img-33.jpg','img/export/img-35.jpg','img/export/img-36.jpg','img/export/img-37.jpg','img/export/img-38.jpg','img/export/img-39.jpg','img/export/img-40.jpg','img/export/img-42.jpg','img/export/img-43.jpg','img/export/img-45.jpg','img/export/img-46.jpg','img/export/img-47.jpg','img/export/img-48.jpg','img/export/img-5.jpg','img/export/img-50.jpg','img/export/img-51.jpg','img/export/img-54.jpg','img/export/img-7.jpg','img/export/img-8.jpg','img/export/img-9.jpg','img/export/img-55.jpg', 'img/export/img-58.jpg'
+  'img/export/a1.jpg',
+  'img/export/a2.jpg',
+  'img/export/a3.jpg',
+  'img/export/bk.jpg',
+  'img/export/a4.jpg',
+  'img/export/a5.jpg',
+  'img/export/a6.jpg',
+  'img/export/a7.jpg',
+  'img/export/a8.jpg',
+  'img/export/a9.jpg',
+  'img/export/a10.jpg',
+  'img/export/a11.jpg',
+  'img/export/a12.jpg',
+  'img/export/a13.jpg',
+  'img/export/a14.jpg',
+  'img/export/a15.jpg',
+  'img/export/a16.jpg',
+  'img/export/a17.jpg',
+  'img/export/a18.jpg',
+  'img/export/a19.jpg',
+  'img/export/a20.jpg',
+  'img/export/a21.jpg',
+  'img/export/img-56.jpg',
+  'img/export/img-57.jpg',
+  'img/export/img-1.jpg',
+  'img/export/img-12.jpg',
+  'img/export/img-13.jpg',
+  'img/export/img-14.jpg',
+  'img/export/img-15.jpg',
+  'img/export/img-16.jpg',
+  'img/export/img-17.jpg',
+  'img/export/img-18.jpg',
+  'img/export/img-19.jpg',
+  'img/export/img-2.jpg',
+  'img/export/img-22.jpg',
+  'img/export/img-23.jpg',
+  'img/export/img-24.jpg',
+  'img/export/img-26.jpg',
+  'img/export/img-27.jpg',
+  'img/export/img-28.jpg',
+  'img/export/img-3.jpg',
+  'img/export/img-32.jpg',
+  'img/export/img-33.jpg',
+  'img/export/img-35.jpg',
+  'img/export/img-36.jpg',
+  'img/export/img-37.jpg',
+  'img/export/img-38.jpg',
+  'img/export/img-39.jpg',
+  'img/export/img-40.jpg',
+  'img/export/img-42.jpg',
+  'img/export/img-43.jpg',
+  'img/export/img-45.jpg',
+  'img/export/img-46.jpg',
+  'img/export/img-47.jpg',
+  'img/export/img-48.jpg',
+  'img/export/img-5.jpg',
+  'img/export/img-50.jpg',
+  'img/export/img-51.jpg',
+  'img/export/img-54.jpg',
+  'img/export/img-7.jpg',
+  'img/export/img-8.jpg',
+  'img/export/img-9.jpg',
+  'img/export/img-55.jpg',
+  'img/export/img-58.jpg'
 ];
 
 let currentIndex = 0;
@@ -38,19 +80,45 @@ function showPreviousImage() {
   document.getElementById('galleryImage').src = images[currentIndex];
 }
 
+// Function to start automatic image change (always clears previous)
+function startAutoChange() {
+  clearInterval(autoChangeInterval);
+  autoChangeInterval = setInterval(showNextImage, 6000); // 6 seconds
+}
+
 // Add event listener for click on the image
 document.querySelector('.image_gallery_click').addEventListener('click', function(event) {
+
   const screenWidth = window.innerWidth;
   const clickX = event.clientX;
 
   if (clickX < screenWidth / 2) {
     // Clicked on the left side
     showPreviousImage();
+    // console.log("previous image");
   } else {
     // Clicked on the right side
     showNextImage();
+    // console.log("next image");
+  }
+
+  // Reset the auto-change timer when user clicks
+  clearInterval(autoChangeInterval);
+  startAutoChange();
+  // console.log("timer reset");
+});
+
+document.querySelector('.image_gallery_click').addEventListener('mousemove', function(event) {
+  const screenWidth = window.innerWidth;
+  const clickX = event.clientX;
+
+  if (clickX < screenWidth / 2) {
+    this.style.cursor = 'w-resize';
+  } else {
+    this.style.cursor = 'e-resize';
   }
 });
+
 
 // Function to toggle grid view
 function toggleGridView() {
@@ -89,11 +157,6 @@ function toggleGridView() {
     // Restart automatic image change when in slideshow view
     startAutoChange();
   }
-}
-
-// Function to start automatic image change
-function startAutoChange() {
-  autoChangeInterval = setInterval(showNextImage, 6000); // Change image every 30 seconds
 }
 
 // Add event listeners for the gallery links
